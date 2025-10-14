2b1d02bc38005a133f84f563be377e9d2466742d
Nasir-afridi <nasirwc05@gmail.com>
2025-10-14 20:50:49 +0500

    added the files

---
+++ Hooks/src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./hook/Main"
+function App() {
+
+  return (
+    <>
+    <Main></Main>
+    </>
+  )
+}
+
+export default App
---
+++ Hooks/src/hook/Main.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+  cons [count , setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  function decrement() {
+    setCount(count - 1)
+  }
+
+  function Reset() {
+    setCount(0)
+  }
+
+
+  return (
+    <>
+    <p>Count is : {count}</p>
+      <button onClick={increment}>Increase</button>
+      <button onClick={decrement}>Decrease</button>
+      <button onClick={Reset}>Reset</button>
+    </>
+  )
+}
+
+export default Main
