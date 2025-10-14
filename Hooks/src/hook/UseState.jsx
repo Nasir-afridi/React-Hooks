be2c303c67119fb26675807dab092ac9eef18387
Nasir-afridi <nasirwc05@gmail.com>
2025-10-14 21:15:27 +0500

    added the like button counter also

--- Hooks/src/hook/UseState.jsx
+++ Hooks/src/hook/UseState.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const UseState = () => {
       const [count , setCount] = useState(0)
+      const [likes , setLikes] = useState(0)
 
   function increment() {
     setCount(count + 1)
@@ -15,12 +16,25 @@ const UseState = () => {
   function Reset() {
     setCount(0)
   }
+
+   function LikesIncre() {
+    setLikes(likes + 1)
+  }
   return (
     <>
         <p>Count is : {count}</p>
         <button onClick={increment}>Increase</button>
         <button onClick={decrement}>Decrease</button>
         <button onClick={Reset}>Reset</button>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
+
+        <p>Total Likes : {likes}</p>
+        <button onClick={LikesIncre}>Likes</button>
     </>
   )
 }
