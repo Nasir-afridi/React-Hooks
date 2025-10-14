721076536d8482146f2fe21f5729c267730e0ab4
Nasir-afridi <nasirwc05@gmail.com>
2025-10-14 20:54:51 +0500

    added the useState hook with the counter example

--- Hooks/src/hook/Main.jsx
+++ Hooks/src/hook/Main.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Main = () => {
-  cons [count , setCount] = useState(0)
+  const [count , setCount] = useState(0)
 
   function increment() {
     setCount(count + 1)
