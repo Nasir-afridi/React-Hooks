3e6db7ee74ce5ed62ab6671506f02dfc3e353f7f
Nasir-afridi <nasirwc05@gmail.com>
2025-10-17 00:39:39 +0500

    receiving props component

---
+++ Hooks/src/hook/UseEffectCounter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UseEffectCounter = ({counter, datas}) => {
+  return (
+    <>
+        <h1>Counter Value is : {counter}</h1>
+        <h2>Data Value is : {datas}</h2>
+    </>
+  )
+}
+
+export default UseEffectCounter
