6e1d0dacabf495b63425de2a7777c38052ce9990
Nasir-afridi <nasirwc05@gmail.com>
2025-10-16 17:15:39 +0500

    format the file

--- Hooks/src/hook/UseEffect.jsx
+++ Hooks/src/hook/UseEffect.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-    /* 
+/* 
     useEffect ek React Hook hai jo side effects handle karta hai
     Jaise: API calls, console logs, event listeners, DOM updates, etc.
     Iske andar likha code component ke render hone ke baad run hota hai
     */
 
-    
-    /* 
+/* 
      Dependency Array ( [] ) ka matlab:
       - Empty array dene ka matlab hai ye effect sirf "component mount" hone par chalega (1 dafa).
       - Re-render hone par ye dobara nahi chalega.
@@ -23,42 +22,40 @@ import { useEffect, useState } from 'react'
       - Iska use tab hota hai jab hume kisi specific state/prop change pe kuch karna ho.
   */
 
-const UseEffect = () => { 
-    const [count, setCount] = useState(0)
-    const [data, setData] = useState(0)
+const UseEffect = () => {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
 
-    useEffect(() => {
-        callOnce()
-        counterUpdate()
-    }, [count]) // empty array mein aik dafa he call hoga jobe call krwaingy haam.
+  useEffect(() => {
+    callOnce();
+    counterUpdate();
+  }, [count]); // empty array mein aik dafa he call hoga jobe call krwaingy haam.
 
-    // count ko sirf count button pr update krna ho to array mein state variable ayga orr function useEffect mein.
-    function counterUpdate() {
-        console.log(count)
-    }
+  // count ko sirf count button pr update krna ho to array mein state variable ayga orr function useEffect mein.
+  function counterUpdate() {
+    console.log(count);
+  }
 
-    function callOnce() {
-        console.log("first callonce called") 
-    }
-    
+  function callOnce() {
+    console.log("first callonce called");
+  }
 
   return (
     <>
-
-        {/* 
+      {/* 
          Button click hone par setCount(1) chalega
          1st click: 0 → 1 (state change hui → re-render hua → callOnce phir chala)
          2nd click: 1 → 1 (state same → React normally re-render nahi karega)
          Lekin development mode (Vite/React 18) me kabhi kabhi extra re-render hota hai
          StrictMode hataane ke baad bhi ye dev environment me ho sakta hai.
          Production build (npm run build) me ye issue nahi hota.
-       */}       
-       
-       <h1>Use</h1>
-       <button onClick={() => setCount(1)}>counter : {count}</button>
-       <button onClick={() => setData(1)}>Data : {data}</button>
-    </>
-  )
-}
+       */}
 
-export default UseEffect
+      <h1>Use</h1>
+      <button onClick={() => setCount(1)}>counter : {count}</button>
+      <button onClick={() => setData(1)}>Data : {data}</button>
+    </>
+  );
+};
+
+export default UseEffect;
