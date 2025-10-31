54a531275a77a36d4138f962a38af03baeeba865
Nasir-afridi <nasirwc05@gmail.com>
2025-10-31 17:13:31 +0500

    Created UseRef component with focus functionality

--- Hooks/src/hook/UseRef.jsx
+++ Hooks/src/hook/UseRef.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useRef } from "react";
 
+// iska use kisi be html element ko control krny kkk liye ya DOM ko manipulate krny kk liye kiya jata hai.
 const UseRef = () => {
+  const inputField = useRef(null);
+  function inputHandler() {
+    console.log(inputField);
+    inputField.current.focus();
+  }
   return (
     <>
+      <h1>Use Ref</h1>
+      <input ref={inputField} type="text" placeholder="Enter user Name" />
+      <button onClick={inputHandler}>Focus on input field</button>
     </>
-  )
-}
+  );
+};
 
-export default UseRef
+export default UseRef;
